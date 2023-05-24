a7c90e5f8633b6162f584ffe403eee460b583ce7
t22007997 <elfamosorayto@gmail.com>
2023-05-24 09:44:06 +0200

    Interface finale et fonctionnelle

--- src/main/java/fr/amu/iut/exercice11/PopulationChartController.java
+++ src/main/java/fr/amu/iut/exercice11/PopulationChartController.java
@@ -4,50 +4,106 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextFlow;
 
-public class PopulationChartController {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+
+public class PopulationChartController implements Initializable {
+
     @FXML
     private BarChart<String, Number> chart;
 
     @FXML
     private CategoryAxis xAxis;
-    @FXML
-    private TextFlow textFlow;
 
-    private ObservableList<XYChart.Data<String, Number>> data;
+    @FXML
+    private NumberAxis yAxis;
 
     @FXML
     private VBox textFieldsContainer;
 
-    private int textFieldCounter = 1;
+    private ObservableList<XYChart.Data<String, Number>> data;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        data = FXCollections.observableArrayList();
+        chart.setData(FXCollections.singletonObservableList(new XYChart.Series<>("Population", data)));
+        xAxis.setLabel("Date");
+
+    }
 
     @FXML
     private void addTextField(ActionEvent event) {
-        HBox textFieldBox = new HBox(10);
-        TextFlow textFieldTextFlow = new TextFlow(new Text("Date" + textFieldCounter + " "));
+        addTextField();
+    }
+
+    private void addTextField() {
+        int textFieldCount = textFieldsContainer.getChildren().size();
+        String label = "Date " + (textFieldCount ) + "   ";
         TextField textField = new TextField();
+        textField.setMaxWidth(100);
+        textField.setTranslateY(-6); // J'ai rajouté ça car sinon le texte est légèrement trop haut par rapport au champs
+        textField.setPromptText("Valeur");
+        textField.setOnKeyPressed(this::handleTextFieldKeyPress);
 
-        textFieldBox.getChildren().addAll(textFieldTextFlow, textField);
+        HBox textFieldBox = new HBox(new Text(label), textField);
         textFieldsContainer.getChildren().add(textFieldBox);
-
-        textFieldCounter++;
     }
 
-    public void initialize() {
-        data = FXCollections.observableArrayList();
-        xAxis.setCategories(FXCollections.<String>observableArrayList());
+    private void handleTextFieldKeyPress(KeyEvent event) {
+        if (event.getCode() == KeyCode.ENTER) {
+            TextField textField = (TextField) event.getSource();
+            String value = textField.getText();
 
-        chart.setData(FXCollections.singletonObservableList(
-                new XYChart.Series<>(data)
-        ));
+            if (!value.isEmpty()) {
+                String date = ((Text) ((HBox) textField.getParent()).getChildren().get(0)).getText().replace(":", "");
+                int parsedValue = parseValue(value);
+                if (parsedValue != -1) {
+                    updateChartData(date, parsedValue);
+                }
+            }
+
+            event.consume();
+        }
     }
 
+    private int parseValue(String value) {
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            return -1;
+        }
+    }
+
+    private void updateChartData(String date, int value) {
+        XYChart.Data<String, Number> newData = new XYChart.Data<>(date, value);
+        int existingIndex = data.indexOf(newData);
+
+        if (existingIndex >= 0) {
+            data.set(existingIndex, newData);
+        } else {
+            data.add(newData);
+
+            List<String> categories = new ArrayList<>(xAxis.getCategories());
+            if (!categories.contains(date)) {
+                categories.add(date);
+                xAxis.setCategories(FXCollections.observableArrayList(categories));
+            }
+        }
+    }
 }
